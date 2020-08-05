0af6cf9b8ab54523eac124ee54b4a0cc667d3a86
13532787 Christopher Kesoglou <37316962+ckesoglou@users.noreply.github.com>
2020-08-05 23:29:32 +1000

    React app now served by express at index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // index simply serves the React site
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 module.exports = router;
