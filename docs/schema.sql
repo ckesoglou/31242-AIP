495ad3426f7d45b338ee4870e3514012690c98a7
13532787 Christopher Kesoglou <37316962+ckesoglou@users.noreply.github.com>
2020-08-08 12:36:35 +1000

    Changed schema proposal based on team comments

--- docs/schema.sql
+++ docs/schema.sql
@@ -2,55 +2,52 @@ CREATE TABLE users (
 	username varchar(16) not null,
 	display_name varchar(50) not null,
 	password_hash char(60) not null,
-	is_admin bit not null default 0,
 	PRIMARY KEY (username)
 );
 
 CREATE TABLE items (
 	id uniqueidentifier not null,
 	display_name varchar(16) not null,
-	is_active bit not null default 1
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE images (
 	id uniqueidentifier not null,
-	blob varbinary(max) not null,
-	PRIMARY KEY (id)
-);
-
-CREATE TABLE favours (
-	id uniqueidentifier,
-	item uniqueidentifier,
-	debtor varchar(16) not null,
-	creditor varchar(16),
-	proof_of_debt uniqueidentifier,
-	proof_of_credit uniqueidentifier,
-	created_timestamp timestamp not null,
-	claimed_timestamp datetime,
-	is_claimed bit not null default 0,
-	FOREIGN KEY (item) REFERENCES items(id),
-	FOREIGN KEY (debtor) REFERENCES users(username),
-	FOREIGN KEY (creditor) REFERENCES users(username),
-	FOREIGN KEY (proof_of_debt) REFERENCES images(id),
-	FOREIGN KEY (proof_of_credit) REFERENCES images(id),
+	blob varbinary(max) not null CONSTRAINT Max_Image_Size CHECK (DATALENGTH(blob) <= 2000000),
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE requests (
 	id uniqueidentifier not null,
-	details varchar(50) not null,
 	author varchar(16) not null,
-	created_timestamp timestamp not null,
-	claimed_timestamp datetime,
-	is_claimed bit not null default 0,
+	completed_by varchar(16),
+	proof_of_completion uniqueidentifier UNIQUE,
+	details varchar(50) not null,
+	created_time datetime not null,
+	completion_time datetime,
+	is_completed bit not null default 0,
 	FOREIGN KEY (author) REFERENCES users(username),
+	FOREIGN KEY (completed_by) REFERENCES users(username),
+	FOREIGN KEY (proof_of_completion) REFERENCES images(id),
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE rewards (
-	request uniqueidentifier not null,
-	favour uniqueidentifier not null,
-	FOREIGN KEY (request) REFERENCES requests(id),
-	FOREIGN KEY (favour) REFERENCES favours(id),
+CREATE TABLE ious (
+	id uniqueidentifier not null,
+	item uniqueidentifier not null,
+	giver varchar(16) not null,
+	receiver varchar(16),
+	parent_request uniqueidentifier,
+	proof_of_debt uniqueidentifier UNIQUE,
+	proof_of_completion uniqueidentifier UNIQUE,
+	created_time datetime not null,
+	claimed_time datetime,
+	is_claimed bit not null default 0,
+	FOREIGN KEY (item) REFERENCES items(id),
+	FOREIGN KEY (giver) REFERENCES users(username),
+	FOREIGN KEY (receiver) REFERENCES users(username),
+	FOREIGN KEY (parent_request) REFERENCES requests(id),
+	FOREIGN KEY (proof_of_debt) REFERENCES images(id),
+	FOREIGN KEY (proof_of_completion) REFERENCES images(id),
+	PRIMARY KEY (id)
 );
