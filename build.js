79fa9b8a66b72aa4c5e586232bce4e6f6fb24fd5
13532787 Christopher Kesoglou <37316962+ckesoglou@users.noreply.github.com>
2020-08-15 18:37:20 +1000

    Modify express to serve react frontend at root

--- build.js
+++ build.js
@@ -5,9 +5,6 @@ const childProcess = require('child_process');
 try {
     // Remove current build
     fs.removeSync('./dist/');
-    // Copy front-end files
-    fs.copySync('./src/public', './dist/public');
-    fs.copySync('./src/views', './dist/views');
     // Transpile the typescript files
     const proc = childProcess.exec('tsc --build tsconfig.prod.json');
     proc.on('close', (code) => {
--- src/Server.ts
+++ src/Server.ts
@@ -51,12 +51,10 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
  *                              Serve front-end content
  ***********************************************************************************/
 
-const viewsDir = path.join(__dirname, 'views');
-app.set('views', viewsDir);
-const staticDir = path.join(__dirname, 'public');
+const staticDir = path.join(__dirname, '../react/build/');
 app.use(express.static(staticDir));
 app.get('*', (req: Request, res: Response) => {
-    res.sendFile('index.html', {root: viewsDir});
+    res.sendFile('index.html', {root: staticDir});
 });
 
 // Export express instance
