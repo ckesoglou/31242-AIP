c4f9519e10a8761c696a83ed354cb9baf5ba7332
seant1 <sean.tran@outlook.com.au>
2020-08-22 22:18:45 +1000

    Set up API call on sign up button click (awaiting API implementation)

---
+++ react/src/api/endpoints.tsx
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.publicapis.org'; // TODO: fix test endpoints after API implementation
+
+export const testEndpoint = baseUrl.concat('/entries?category=animals&https=true'); // TODO: fix test endpoints after API implementation
