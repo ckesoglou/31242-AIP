036c1f19e6aff11bc9ce353a5f20af18bd776547
Piploop <james.lee9112000@gmail.com>
2020-08-26 00:21:47 +1000

    Added BackEnd RestfulAPI for SignUp feature

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import UserRouter from './Users';
+import SignUpRouter from './SignUp';
 
 // Init router and path
 const router = Router();
 
 // Add sub-routes
 router.use('/users', UserRouter);
+router.use('/signup', SignUpRouter);
 
 // Export the base-router
 export default router;
