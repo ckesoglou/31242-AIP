9cddc0c3fd569cf3efcf7ad4f01dba23ee812756
kevinleung56 <kevinleung56@gmail.com>
2020-08-26 22:44:07 +1000

    Add loading circle for sign in + add some styling

--- react/src/components/App.tsx
+++ react/src/components/App.tsx
@@ -3,85 +3,55 @@ import "../assets/css/App.css";
 import {
   Switch,
   Route,
-  Link,
   BrowserRouter as Router,
   Redirect,
-  RouteComponentProps,
+  Link,
 } from "react-router-dom";
 import Login from "../pages/login";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/about">About</Link>
-        <Link to="/users">User</Link>
-        <Link to="/home">Home</Link>
-        <Link to="/">Login</Link>
-
-        <Switch>
-          <Route path="/about">
-            <h1>About!</h1>
-          </Route>
-          <Route path="/users">
-            <h1>Users!</h1>
-          </Route>
-          <ProtectedRoute path="/home" component={Home}></ProtectedRoute>
-          <Route path="/" component={Login} />
-        </Switch>
-      </div>
+      <Switch>
+        <ProtectedRoute path="/users"></ProtectedRoute>
+        <ProtectedRoute path="/home">
+          <h1>You've reached the next page!</h1>
+          <h2>
+            <Link to="/">Click here to go back!</Link>
+          </h2>
+        </ProtectedRoute>
+        <Route path="/" component={Login} />
+      </Switch>
     </Router>
   );
 }
 
-interface HomeProps extends RouteComponentProps {}
-
-type HomeState = {
-  username: string;
-};
-
-class Home extends React.Component<HomeProps, HomeState> {
-  constructor(props: HomeProps) {
-    super(props);
-    this.state = {
-      username: "",
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Home!</h1>
-      </div>
-    );
-  }
-}
-
 // mock auth
-const fakeAuth = {
+const Authentication = {
   isAuthenticated: false,
   authenticate(cb: () => void) {
     this.isAuthenticated = true;
-    setTimeout(cb, 100); // fake async
+    setTimeout(cb, 3000); // fake async
   },
   signout(cb: () => void) {
     this.isAuthenticated = false;
-    setTimeout(cb, 100);
+    setTimeout(cb, 3000);
   },
 };
 
+// thanks to some random person
 const ProtectedRoute: React.ComponentType<any> = ({
   component: Component,
   ...rest
 }) => {
   // remove below
-  fakeAuth.authenticate(() => {});
+  Authentication.authenticate(() => {});
   return (
     <Route
       {...rest}
       render={(props) =>
         // add api get check cookie
-        fakeAuth.isAuthenticated ? (
+        Authentication.isAuthenticated ? (
           <Component {...props} />
         ) : (
           <Redirect
