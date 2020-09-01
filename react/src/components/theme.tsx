37dc27ba7cfa2ab379581305cb989f76f28ef9f6
seant1 <sean.tran@outlook.com.au>
2020-09-02 01:50:44 +1000

    Create material UI colour themes

--- react/src/components/theme.tsx
+++ react/src/components/theme.tsx
@@ -19,4 +19,76 @@ const theme = createMuiTheme({
   },
 });
 
-export default theme;
+const themeDark = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#BB86FC",
+    },
+    secondary: {
+      main: "#03DAC5",
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: "#121212",
+    },
+  },
+});
+
+const themeApple = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#86C232",
+    },
+    secondary: {
+      main: "#707793",
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: "#222629",
+    },
+  },
+});
+
+const themeMint = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#3BBA9C",
+    },
+    secondary: {
+      main: "#707793",
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: "#2E3047",
+    },
+  },
+});
+
+const themeBlue = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#2970ff",
+    },
+    secondary: {
+      main: "#2dd8a3",
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: "#152042",
+    },
+  },
+});
+
+export default themeMint;
