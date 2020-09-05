2c55d144575b2ae53501fd1a8c30be47b093d3da
seant1 <sean.tran@outlook.com.au>
2020-09-06 01:53:22 +1000

    Refactor themes implementation to remove warnings

--- react/src/components/theme.tsx
+++ react/src/components/theme.tsx
@@ -1,94 +1,95 @@
 import { red } from "@material-ui/core/colors";
 import { createMuiTheme } from "@material-ui/core/styles";
 
-// A custom theme for this app
+enum Theme {
+  light,
+  dark,
+  mint,
+  apple,
+  blue,
+}
+
+// set current theme
+let currentTheme = Theme.apple;
+
+function colorType(): "light" | "dark" {
+  switch (currentTheme) {
+    case Theme.light:
+      return "light";
+    case Theme.dark:
+    case Theme.mint:
+    case Theme.apple:
+    case Theme.blue:
+      return "dark";
+    default:
+      return "light";
+  }
+}
+function primaryColor(): string {
+  switch (currentTheme) {
+    case Theme.light:
+      return "#556cd6";
+    case Theme.dark:
+      return "#BB86FC";
+    case Theme.mint:
+      return "#3BBA9C";
+    case Theme.apple:
+      return "#86C232";
+    case Theme.blue:
+      return "#2970ff";
+    default:
+      return "#556cd6";
+  }
+}
+function secondaryColor(): string {
+  switch (currentTheme) {
+    case Theme.light:
+      return "#19857b";
+    case Theme.dark:
+      return "#03DAC5";
+    case Theme.mint:
+      return "#707793";
+    case Theme.apple:
+      return "#707793";
+    case Theme.blue:
+      return "#2dd8a3";
+    default:
+      return "#19857b";
+  }
+}
+function backgroundColor(): string {
+  switch (currentTheme) {
+    case Theme.light:
+      return "#fff";
+    case Theme.dark:
+      return "#121212";
+    case Theme.mint:
+      return "#2E3047";
+    case Theme.apple:
+      return "#222629";
+    case Theme.blue:
+      return "#152042";
+    default:
+      return "#fff";
+  }
+}
+
 const theme = createMuiTheme({
   palette: {
+    type: colorType(),
     primary: {
-      main: "#556cd6",
+      main: primaryColor(),
     },
     secondary: {
-      main: "#19857b",
+      main: secondaryColor(),
     },
     error: {
       main: red.A400,
     },
     background: {
-      default: "#fff",
+      default: backgroundColor(),
     },
   },
 });
 
-const themeDark = createMuiTheme({
-  palette: {
-    type: "dark",
-    primary: {
-      main: "#BB86FC",
-    },
-    secondary: {
-      main: "#03DAC5",
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: "#121212",
-    },
-  },
-});
-
-const themeApple = createMuiTheme({
-  palette: {
-    type: "dark",
-    primary: {
-      main: "#86C232",
-    },
-    secondary: {
-      main: "#707793",
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: "#222629",
-    },
-  },
-});
-
-const themeMint = createMuiTheme({
-  palette: {
-    type: "dark",
-    primary: {
-      main: "#3BBA9C",
-    },
-    secondary: {
-      main: "#707793",
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: "#2E3047",
-    },
-  },
-});
-
-const themeBlue = createMuiTheme({
-  palette: {
-    type: "dark",
-    primary: {
-      main: "#2970ff",
-    },
-    secondary: {
-      main: "#2dd8a3",
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: "#152042",
-    },
-  },
-});
-
-export default themeMint;
+export default theme;
