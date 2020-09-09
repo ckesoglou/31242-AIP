fb8c68f7542f70f7d10e08086b7517ebe404450e
hjsith <hasithjayasekera@gmail.com>
2020-09-09 13:20:45 +1000

    Database connection

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,11 @@
 import { Router } from "express";
 import UserRouter from "./Users";
-import ImageRouter from "./Images";
 
 // Init router and path
 const router = Router();
 
 // Add sub-routes
 router.use("/users", UserRouter);
-router.use("/images", ImageRouter);
 
 // Export the base-router
 export default router;
