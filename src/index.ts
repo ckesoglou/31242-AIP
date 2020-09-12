3108f12351b76c3a7066157af249b1f85ec2881b
hjsith <hasithjayasekera@gmail.com>
2020-09-13 00:53:43 +1000

    Update

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,17 @@ import db from "./daos/DBInstance";
 
 // Start the server
 const port = Number(env.port || 3000);
-db.authenticate();
+
+async function dbconnect() {
+  try {
+    await db.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+
+dbconnect();
 
 app.listen(port, () => {
   logger.info("Express server started on port: " + port);
