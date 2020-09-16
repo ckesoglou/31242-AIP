ad70e06ee2ed02563386b8390b94cfaaa8671198
kevinleung56 <kevinleung56@gmail.com>
2020-09-16 20:52:24 +1000

    Create endpoint + set up for front-end api calls

--- react/src/api/endpoints.tsx
+++ react/src/api/endpoints.tsx
@@ -2,3 +2,4 @@ export const baseUrl = "api";
 
 export const loginEndpoint = baseUrl.concat("/login");
 export const signUpEndpoint = baseUrl.concat("/signup");
+export const userProfileEndpoint = baseUrl.concat("/user");
