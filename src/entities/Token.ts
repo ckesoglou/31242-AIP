3dbbc191dd2d496834e1571828ce4f2235d5159d
13532787 Christopher Kesoglou <37316962+ckesoglou@users.noreply.github.com>
2020-09-24 12:39:43 +1000

    Removed unneccesary constructor comment on Token

--- src/entities/Token.ts
+++ src/entities/Token.ts
@@ -14,21 +14,6 @@ class Token extends Model<ITokenAttributes> implements ITokenAttributes {
   public device_name!: string;
   public created_time!: Date;
   public expiry_time!: Date;
-
-  // constructor(
-  //   refresh_token: string,
-  //   username: string,
-  //   device_name: string,
-  //   created_time: Date,
-  //   expiry_time: Date
-  // ) {
-  //   super();
-  //   this.refresh_token = refresh_token;
-  //   this.username = username;
-  //   this.device_name = device_name;
-  //   this.created_time = created_time;
-  //   this.expiry_time = expiry_time;
-  // }
 }
 
 export default Token;
