e10a8e48e58f4be3edaae69646320ca4639807ff
kevinleung56 <kevinleung56@gmail.com>
2020-09-27 15:09:24 +1000

    Further work on tests + got snackbar working

--- react/src/pages/userprofile.test.tsx
+++ react/src/pages/userprofile.test.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { shallow, mount } from "enzyme";
-
 import UserProfile from "./userprofile";
-import { MemoryRouter, Route } from "react-router-dom";
 
 const testProps = {
   history: {} as any,
@@ -12,6 +10,7 @@ const testProps = {
 
 describe("UserProfile", () => {
   it("should render correctly", () => {
+    // need to provide context
     const component = shallow(
       <UserProfile
         history={testProps.history}
@@ -24,8 +23,7 @@ describe("UserProfile", () => {
   });
 
   it("should handle new request if button is clicked", () => {
-    const spy = jest.spyOn(UserProfile.prototype, "handleSignUp");
-    // this needs to be fixed
+    const spy = jest.spyOn(UserProfile.prototype, "fetchNewRequest");
     const wrapper = mount(
       <UserProfile
         history={testProps.history}
@@ -34,12 +32,18 @@ describe("UserProfile", () => {
       />
     );
 
-    wrapper.find("button").simulate("click");
+    wrapper
+      .find("input#favourText")
+      .simulate("change", { target: { value: "Ben!" } });
+    wrapper
+      .find("input#rewardText")
+      .simulate("change", { target: { value: "Johnston!" } });
+    wrapper.find("button#createRequest").simulate("click");
 
     expect(spy).toHaveBeenCalledTimes(1);
   });
 
-  it("should refresh tab contents if button is clicked", () => {
+  it("should have loading circle on tab contents if button is clicked", () => {
     const spy = jest.spyOn(UserProfile.prototype, "fetchAllTabs");
     const wrapper = mount(
       <UserProfile
@@ -49,8 +53,10 @@ describe("UserProfile", () => {
       />
     );
 
-    wrapper.find("button").simulate("click");
+    wrapper.find("button#createRequest").simulate("click");
+    let loadingCircle = wrapper.find("loading").length;
 
     expect(spy).toHaveBeenCalledTimes(1);
+    expect(loadingCircle).toBe(1);
   });
 });
