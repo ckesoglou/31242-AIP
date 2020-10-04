ec061ffb3a1d3c0f0ed5be0a7baf373f9bd445ac
kevinleung56 <kevinleung56@gmail.com>
2020-10-04 19:19:41 +1100

    Create avatar images + working on user menu

--- react/src/pages/home.tsx
+++ react/src/pages/home.tsx
@@ -1,20 +1,35 @@
 import React from "react";
 import { RouteComponentProps, Link as RouterLink } from "react-router-dom";
-import "../assets/css/userprofile.css";
+import "../assets/css/home.css";
 import {
+  Avatar,
   Box,
+  Button,
   Container,
   Grid,
   InputAdornment,
   Link,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+  MenuProps,
   Paper,
   TextField,
   Typography,
+  withStyles,
 } from "@material-ui/core";
 import { UserContext } from "../components/user-context";
 import SearchIcon from "@material-ui/icons/Search";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import SendIcon from "@material-ui/icons/Send";
 
-type HomeState = {};
+type HomeState = {
+  initials: string;
+  userMenu: boolean;
+  anchorEl: HTMLElement | null;
+};
 
 interface IHomeProps extends RouteComponentProps {
   location: {
@@ -30,17 +45,66 @@ interface IHomeProps extends RouteComponentProps {
   };
 }
 
+const StyledMenu = withStyles({
+  paper: {
+    border: "1px solid #d3d4d5",
+  },
+})((props: MenuProps) => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: "bottom",
+      horizontal: "center",
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "center",
+    }}
+    {...props}
+  />
+));
+
+const StyledMenuItem = withStyles((theme) => ({
+  root: {
+    "&:focus": {
+      backgroundColor: theme.palette.primary.main,
+      "& .MuiListItemIcon-root, & .MuiListItemText-primary": {
+        color: theme.palette.common.white,
+      },
+    },
+  },
+}))(MenuItem);
+
 class Home extends React.Component<IHomeProps, HomeState> {
   constructor(props: IHomeProps) {
     super(props);
   }
 
-  state: HomeState = {};
+  state: HomeState = {
+    initials: "",
+    userMenu: false,
+    anchorEl: null,
+  };
 
   static contextType: React.Context<{
     user: {};
     updateUser: (newUser: object) => void;
   }> = UserContext;
+
+  // most readable solution from https://stackoverflow.com/questions/33076177/getting-name-initials-using-js
+  nameToInitials(fullName: string) {
+    const namesArray = fullName.split(" ");
+    if (namesArray.length === 1) return `${namesArray[0].charAt(0)}`;
+    else
+      return `${namesArray[0].charAt(0)}${namesArray[
+        namesArray.length - 1
+      ].charAt(0)}`;
+  }
+
+  componentDidMount() {
+    this.setState({ initials: this.nameToInitials(this.context.user.name) });
+  }
 
   render() {
     return (
@@ -55,9 +119,42 @@ class Home extends React.Component<IHomeProps, HomeState> {
                 <Typography component="h1" variant="h4">
                   {"Home"}
                 </Typography>
-                <Typography component="h3" variant="h4">
-                  {"Placeholder Image"}
-                </Typography>
+                <Avatar
+                  onClick={() => {
+                    this.setState({ userMenu: true });
+                  }}
+                  id="avatar"
+                >
+                  {this.state.initials}
+                </Avatar>
+                <StyledMenu
+                  id="customized-menu"
+                  anchorEl={this.state.anchorEl}
+                  keepMounted
+                  open={this.state.userMenu}
+                  onClose={() => {
+                    this.setState({ userMenu: false });
+                  }}
+                >
+                  <StyledMenuItem>
+                    <ListItemIcon>
+                      <SendIcon fontSize="small" />
+                    </ListItemIcon>
+                    <ListItemText primary="Sent mail" />
+                  </StyledMenuItem>
+                  <StyledMenuItem>
+                    <ListItemIcon>
+                      <DraftsIcon fontSize="small" />
+                    </ListItemIcon>
+                    <ListItemText primary="Drafts" />
+                  </StyledMenuItem>
+                  <StyledMenuItem>
+                    <ListItemIcon>
+                      <InboxIcon fontSize="small" />
+                    </ListItemIcon>
+                    <ListItemText primary="Inbox" />
+                  </StyledMenuItem>
+                </StyledMenu>
               </div>
               <h1>You've reached the next page!</h1>
               <h2>
@@ -79,12 +176,11 @@ class Home extends React.Component<IHomeProps, HomeState> {
                 <Typography component="h3" variant="h4">
                   {"Requests"}
                 </Typography>
-                <Box display="inline">
-                  <Typography component="h4" variant="h5">
+                <Box id="filterContainer" display="inline">
+                  <Typography id="filter-text" component="h4" variant="h5">
                     {"Filter"}
                   </Typography>
                   <TextField
-                    id="filter-input"
                     InputProps={{
                       startAdornment: (
                         <InputAdornment position="start">
@@ -93,6 +189,14 @@ class Home extends React.Component<IHomeProps, HomeState> {
                       ),
                     }}
                   />
+                  <Button
+                    id="filter-button"
+                    size="large"
+                    variant="contained"
+                    color="primary"
+                  >
+                    <label>Filter</label>
+                  </Button>
                 </Box>
               </div>
             </Grid>
