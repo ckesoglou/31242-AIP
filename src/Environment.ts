f80e504860a4e2f87ff282861efe88f93c536839
13532787 Christopher Kesoglou <37316962+ckesoglou@users.noreply.github.com>
2020-10-08 13:42:37 +1100

    Removed duplicate files

--- src/Environment.ts
+++ src/Environment.ts
@@ -2,10 +2,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-  port: process.env.PORT ?? "3000",
+  port: process.env.PORT ?? "4000",
   db_host: process.env.DB_HOST ?? "localhost",
   db_username: process.env.DB_USERNAME ?? "",
   db_password: process.env.DB_PASSWORD ?? "",
-  db_name: process.env.DB_NAME ?? "ioweyou",
+  db_name: process.env.DB_NAME ?? "",
   jwt_secret: process.env.JWT_SECRET ?? "ioweyou.tech",
 };
