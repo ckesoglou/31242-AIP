44a0ea9e771ed8afc58ecd69636a29b6998bcc3c
Christopher Kesoglou <37316962+ckesoglou@users.noreply.github.com>
2020-10-08 22:41:29 +1100

    Changed default environment port to 3000

--- src/Environment.ts
+++ src/Environment.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-  port: process.env.PORT ?? "4000",
+  port: process.env.PORT ?? "3000",
   db_host: process.env.DB_HOST ?? "localhost",
   db_username: process.env.DB_USERNAME ?? "",
   db_password: process.env.DB_PASSWORD ?? "",
