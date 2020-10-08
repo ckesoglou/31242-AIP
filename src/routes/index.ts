7fa69a800846aedeaae47934397a06fc02b422b3
Piploop <james.lee9112000@gmail.com>
2020-10-08 23:43:39 +1100

    Added signup to routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import LoginRouter from "./Login";
+import SignUpRouter from './SignUp';
 
 // Init router and path
 const router = Router();
 
 // Add sub-routes
 router.use("/login", LoginRouter);
+router.use("/signup", SignUpRouter);
 
 // Export the base-router
 export default router;
