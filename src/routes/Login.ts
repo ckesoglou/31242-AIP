45a37bd4d603d631b377c9aa68fcb141fff879d5
13532787 Christopher Kesoglou <37316962+ckesoglou@users.noreply.github.com>
2020-10-08 11:33:52 +1100

    Refactored out token generation logic to shared Authenticate function

--- src/routes/Login.ts
+++ src/routes/Login.ts
@@ -3,12 +3,7 @@ import { BAD_REQUEST, OK, UNAUTHORIZED } from "http-status-codes";
 import { getUser } from "../daos/Users";
 import Joi, { string, ObjectSchema } from "joi";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import { v4 as uuid } from "uuid";
-import env from "../Environment";
-import User from "../entities/User";
-import Token from "@entities/Token";
-import { createToken } from "@daos/Tokens";
+import { generateNewAuthenticationTokens } from "@shared/Authenticate";
 
 const router = Router();
 
@@ -31,7 +26,7 @@ router.post("/", async (req: Request, res: Response) => {
 
   if (error) {
     return res.status(BAD_REQUEST).json({
-      error: error.message,
+      errors: [error.message],
     });
   }
 
@@ -45,42 +40,18 @@ router.post("/", async (req: Request, res: Response) => {
     );
 
     if (match) {
-      const now = new Date();
-      const monthFromNow = new Date();
-      monthFromNow.setDate(monthFromNow.getDate() + 30);
-
-      const serverRefreshToken = await createToken(
-        uuid(),
-        user.username,
+      await generateNewAuthenticationTokens(
+        user,
         req.headers.host ?? "Unknown",
-        now,
-        monthFromNow
+        res
       );
-      const clientRefreshToken = jwt.sign(
-        {
-          username: user.username,
-          token: serverRefreshToken.refresh_token,
-        },
-        env.jwt_secret,
-        { expiresIn: "30 days" }
-      );
-      const clientAccessToken = jwt.sign(
-        { username: user.username },
-        env.jwt_secret,
-        { expiresIn: "30m" }
-      );
-
-      res.cookie("access_tokens", {
-        access_token: clientAccessToken,
-        refresh_token: clientRefreshToken,
-      });
 
       return res.status(OK).end();
     }
   }
 
   return res.status(UNAUTHORIZED).json({
-    error: "Username or password was incorrect", // We do not specify which for security reasons
+    errors: ["Username or password was incorrect"], // We do not specify which for security reasons
   });
 });
 
