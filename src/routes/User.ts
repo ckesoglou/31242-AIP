f49c8aba7bbff3c33905bbaee917f8dd1da7b8a9
hjsith <hasithjayasekera@gmail.com>
2020-10-11 14:23:27 +1100

    Create user profile retrieval

---
+++ src/routes/User.ts
@@ -0,0 +1,26 @@
+import { Request, response, Response, Router } from "express";
+import { BAD_REQUEST, OK, UNAUTHORIZED } from "http-status-codes";
+import { getUser } from "../daos/Users";
+import { getAuthenticatedUser } from "@shared/Authenticate";
+import User from "@entities/User";
+
+const router = Router();
+
+router.get("/", async (req: Request, res: Response) => {
+  const localUser = await getAuthenticatedUser(req, res);
+  if (localUser) {
+    const user = await getUser(localUser.username);
+    if (!user) {
+      return res.status(401).json({
+        error: ["User no longer exists"],
+      });
+    }
+    return res.status(OK).json({ user });
+  } else {
+    return res.status(401).json({
+      error: ["Invalid session"],
+    });
+  }
+});
+
+export default router;
