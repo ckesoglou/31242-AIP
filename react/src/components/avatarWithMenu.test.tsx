18904daf107c9d517167dbcf1845d1f7f10c16c2
kevinleung56 <kevinleung56@gmail.com>
2020-10-19 22:53:44 +1100

    Fix warnings v2

--- react/src/components/avatarWithMenu.test.tsx
+++ react/src/components/avatarWithMenu.test.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { AvatarWithMenu } from "./avatarWithMenu";
+import { UserContext } from "./user-context";
 
 // Integration tests are on home.test.tsx
 describe("<AvatarWithMenu />", () => {
   it("should render correctly", () => {
     const wrapper = shallow(
-      <AvatarWithMenu loggedIn={true} fullName={"Ben Johnston"} />
+      <UserContext.Provider
+        value={{
+          user: { name: "Kevin Leung" },
+          updateUser: () => {},
+        }}
+      >
+        <AvatarWithMenu />
+      </UserContext.Provider>
     );
 
     expect(wrapper).toMatchSnapshot();
