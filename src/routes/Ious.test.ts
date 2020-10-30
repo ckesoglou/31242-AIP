8826938a4836ac088a2d05c43327569a60043786
seant1 <sean.tran@outlook.com.au>
2020-10-30 21:04:15 +1100

    Refactor login unit tests

--- src/routes/Ious.test.ts
+++ src/routes/Ious.test.ts
@@ -1,12 +1,6 @@
 /// <reference types="jest" />
 const request = require("supertest");
-// const app = require("../Server");
 const APP = "https://ioweyou.tech";
-
-// import sum from "./functions";
-// test("Adds 2 + 2 to equal 4", () => {
-//   expect(sum(2, 2)).toBe(4);
-// });
 
 describe("Login endpoint", () => {
   it("should login user with correct credentials", async () => {
@@ -17,54 +11,75 @@ describe("Login endpoint", () => {
     expect(res.statusCode).toEqual(200);
   });
 
-  it("should return error with wrong password", async () => {
-    const res = await request(APP).post("/api/login").send({
-      username: "sean",
-      password: "wrongpassword",
+  describe("Unauthorised", () => {
+    it("should return error with wrong password", async () => {
+      const res = await request(APP).post("/api/login").send({
+        username: "sean",
+        password: "wrongpassword",
+      });
+      expect(res.statusCode).toEqual(401);
+      expect(res.body).toHaveProperty("errors");
+      expect(res.body.errors).toContain("Username or password was incorrect");
     });
-    expect(res.statusCode).toEqual(401);
-    expect(res.body).toHaveProperty("errors");
-    expect(res.body.errors).toContain("Username or password was incorrect");
+
+    it("should return error with nonexistent username", async () => {
+      const res = await request(APP).post("/api/login").send({
+        username: "seanasdfasdfasdf",
+        password: "Seansean!",
+      });
+      expect(res.statusCode).toEqual(401);
+      expect(res.body).toHaveProperty("errors");
+      expect(res.body.errors).toContain("Username or password was incorrect");
+    });
   });
 
-  it("should return error with nonexistent username", async () => {
-    const res = await request(APP).post("/api/login").send({
-      username: "seanasdfasdfasdf",
-      password: "Seansean!",
+  describe("Validation", () => {
+    it("should validate username max field length", async () => {
+      const res = await request(APP).post("/api/login").send({
+        username: "seanmorethan16characters",
+        password: "Seansean!",
+      });
+      expect(res.statusCode).toEqual(400);
+      expect(res.body).toHaveProperty("errors");
+      expect(res.body.errors).toContain(
+        '"username" length must be less than or equal to 16 characters long'
+      );
     });
-    expect(res.statusCode).toEqual(401);
-    expect(res.body).toHaveProperty("errors");
-    expect(res.body.errors).toContain("Username or password was incorrect");
-  });
 
-  it("should validate username field length", async () => {
-    const res = await request(APP).post("/api/login").send({
-      username: "seanmorethan16characters",
-      password: "Seansean!",
+    it("should validate username min field length", async () => {
+      const res = await request(APP).post("/api/login").send({
+        username: "s",
+        password: "Seansean!",
+      });
+      expect(res.statusCode).toEqual(400);
+      expect(res.body).toHaveProperty("errors");
+      expect(res.body.errors).toContain(
+        '"username" length must be at least 2 characters long'
+      );
     });
-    expect(res.statusCode).toEqual(400);
-    expect(res.body).toHaveProperty("errors");
-    expect(res.body.errors).toContain(
-      '"username" length must be less than or equal to 16 characters long'
-    );
-  });
 
-  it("should validate username field valid characters", async () => {
-    const res = await request(APP).post("/api/login").send({
-      username: "sean#",
-      password: "Seansean!",
+    it("should validate username field valid characters", async () => {
+      const nonAlphaNumeric = "!@#$%^&*()_+-=`~[{]}\\|;:'\",<.>/?";
+      for (let character of nonAlphaNumeric) {
+        const res = await request(APP).post("/api/login").send({
+          username: character,
+          password: "Seansean!",
+        });
+        expect(res.statusCode).toEqual(400);
+        expect(res.body).toHaveProperty("errors");
+        expect(res.body.errors).toContain(
+          '"username" must only contain alpha-numeric characters'
+        );
+      }
     });
-    expect(res.statusCode).toEqual(400);
-    expect(res.body).toHaveProperty("errors");
-    expect(res.body.errors).toContain(
-      '"username" must only contain alpha-numeric characters'
-    );
+
+    it("should validate password field", async () => {
+      const res = await request(APP).post("/api/login").send({
+        username: "sean",
+      });
+      expect(res.statusCode).toEqual(400);
+      expect(res.body).toHaveProperty("errors");
+      expect(res.body.errors).toContain('"password" is required');
+    });
   });
 });
-// describe("Ious Endpoints", () => {
-//   it("should get owed ious", async () => {
-//     const res = await request(app).get("/api/iou/owed");
-//     expect(res.statusCode).toEqual(200);
-//     // expect(res.body).toHaveProperty("post");
-//   });
-// });
