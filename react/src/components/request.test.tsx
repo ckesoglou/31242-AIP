5d01573bc8aef48c49040d5694660be9cf9b3c8c
Piploop <james.lee9112000@gmail.com>
2020-10-31 20:10:13 +1100

    Write unit test for request component

--- react/src/components/request.test.tsx
+++ react/src/components/request.test.tsx
@@ -8,18 +8,31 @@ describe("<Request />", () => {
       <Request
         request={{
           id: "1",
-          author: { username: "James", display_name: "James" },
-          completed_by: { username: "Kevin", display_name: "Kevin" },
+          author: { username: "James", display_name: "James Lee" },
+          completed_by: { username: "Kevin", display_name: "Kevin Leung" },
           proof_of_completion: "Some ID",
           rewards: [
-            { id: "1", display_name: "Hug" },
-            { id: "2", display_name: "Coffee" },
+            {
+              id: "1",
+              giver: { username: "James", display_name: "James Lee" },
+              item: { id: "202", display_name: "Hug" },
+            },
+            {
+              id: "2",
+              giver: { username: "Sean", display_name: "Sean Tran" },
+              item: { id: "201", display_name: "Coffee" },
+            },
           ],
           details: "Clean the fridge",
-          created_time: "02/02/2020",
-          comletion_time: "02/02/2020",
+          created_time: "01/01/2020",
+          completion_time: "02/02/2020",
           is_completed: true,
         }}
+        potentialRewards={[
+          { id: "201", display_name: "Coffee" },
+          { id: "202", display_name: "Hug" },
+        ]}
+        iouType={2}
       />
     );
     expect(wrapper).toMatchSnapshot();
