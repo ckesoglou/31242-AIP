cedd398bd2d4188be08d89b2f9581dcb4029c8ca
seant1 <sean.tran@outlook.com.au>
2020-10-31 14:34:35 +1100

    Add SignUp unit tests

--- src/routes/SignUp.test.ts
+++ src/routes/SignUp.test.ts
@@ -1,4 +1,7 @@
 /// <reference types="jest" />
+
+import sequelize from "@daos/DBInstance";
+
 const request = require("supertest");
 const APP = "https://ioweyou.tech";
 const TESTUSER = {
@@ -8,58 +11,55 @@ const TESTUSER = {
 };
 
 beforeAll(async () => {
-  await request(APP)
-    .post("/api/signup")
-    .send({
-      username: `${TESTUSER.username}`,
-      displayName: `${TESTUSER.displayName}`,
-      password: `${TESTUSER.password}`,
-    });
+  await sequelize.query(
+    `DELETE from tokens WHERE username='${TESTUSER.username}';
+      DELETE from users WHERE username='${TESTUSER.username}';`
+  );
 });
 
-describe("Login endpoint", () => {
-  it("should login user with correct credentials", async () => {
-    const res = await request(APP)
-      .post("/api/login")
-      .send({
-        username: `${TESTUSER.username}`,
-        password: `${TESTUSER.password}`,
-      });
-    expect(res.statusCode).toEqual(200);
-  });
-
-  describe("Unauthorised", () => {
-    it("should return error with wrong password", async () => {
+describe("Signup endpoint", () => {
+  describe("Successful signup", () => {
+    it("should sign up new user", async () => {
+      const res = await request(APP)
+        .post("/api/signup")
+        .send({
+          username: `${TESTUSER.username}`,
+          displayName: `${TESTUSER.displayName}`,
+          password: `${TESTUSER.password}`,
+        });
+      expect(res.statusCode).toEqual(201);
+    });
+    it("should login user with sign up credentials", async () => {
       const res = await request(APP)
         .post("/api/login")
         .send({
           username: `${TESTUSER.username}`,
-          password: "wrongpassword",
-        });
-      expect(res.statusCode).toEqual(401);
-      expect(res.body).toHaveProperty("errors");
-      expect(res.body.errors).toContain("Username or password was incorrect");
-    });
-
-    it("should return error with nonexistent username", async () => {
-      const res = await request(APP)
-        .post("/api/login")
-        .send({
-          username: "seanasdfasdfasdf",
           password: `${TESTUSER.password}`,
         });
-      expect(res.statusCode).toEqual(401);
-      expect(res.body).toHaveProperty("errors");
-      expect(res.body.errors).toContain("Username or password was incorrect");
+      expect(res.statusCode).toEqual(200);
     });
   });
 
   describe("Validation", () => {
+    it("should validate usernames already taken", async () => {
+      const res = await request(APP)
+        .post("/api/signup")
+        .send({
+          username: `${TESTUSER.username}`,
+          displayName: `${TESTUSER.displayName}`,
+          password: `${TESTUSER.password}`,
+        });
+      expect(res.statusCode).toEqual(422);
+      expect(res.body).toHaveProperty("errors");
+      expect(res.body.errors).toContain("Username already taken.");
+    });
+
     it("should validate username max field length", async () => {
       const res = await request(APP)
-        .post("/api/login")
+        .post("/api/signup")
         .send({
-          username: "seanmorethan16characters",
+          username: "testmorethan16characters",
+          displayName: `${TESTUSER.displayName}`,
           password: `${TESTUSER.password}`,
         });
       expect(res.statusCode).toEqual(400);
@@ -71,9 +71,10 @@ describe("Login endpoint", () => {
 
     it("should validate username min field length", async () => {
       const res = await request(APP)
-        .post("/api/login")
+        .post("/api/signup")
         .send({
           username: "s",
+          displayName: `${TESTUSER.displayName}`,
           password: `${TESTUSER.password}`,
         });
       expect(res.statusCode).toEqual(400);
@@ -87,9 +88,10 @@ describe("Login endpoint", () => {
       const nonAlphaNumeric = "!@#$%^&*()_+-=`~[{]}\\|;:'\",<.>/?";
       for (let character of nonAlphaNumeric) {
         const res = await request(APP)
-          .post("/api/login")
+          .post("/api/signup")
           .send({
             username: character,
+            displayName: `${TESTUSER.displayName}`,
             password: `${TESTUSER.password}`,
           });
         expect(res.statusCode).toEqual(400);
@@ -102,13 +104,26 @@ describe("Login endpoint", () => {
 
     it("should validate password field", async () => {
       const res = await request(APP)
-        .post("/api/login")
+        .post("/api/signup")
         .send({
           username: `${TESTUSER.username}`,
+          displayName: `${TESTUSER.displayName}`,
         });
       expect(res.statusCode).toEqual(400);
       expect(res.body).toHaveProperty("errors");
       expect(res.body.errors).toContain('"password" is required');
+    });
+
+    it("should validate display name field", async () => {
+      const res = await request(APP)
+        .post("/api/signup")
+        .send({
+          username: `${TESTUSER.username}`,
+          password: `${TESTUSER.password}`,
+        });
+      expect(res.statusCode).toEqual(400);
+      expect(res.body).toHaveProperty("errors");
+      expect(res.body.errors).toContain('"displayName" is required');
     });
   });
 });
