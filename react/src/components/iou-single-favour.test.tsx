bb4ec3e656e2493eed828361cb6584998729bbd5
Piploop <james.lee9112000@gmail.com>
2020-10-31 18:31:53 +1100

    Write unit tests for IOU favour component

--- react/src/components/iou-single-favour.test.tsx
+++ react/src/components/iou-single-favour.test.tsx
@@ -6,11 +6,37 @@ describe("<IouFavour />", () => {
   it("should render correctly", () => {
     const wrapper = shallow(
       <IouFavour
-        giverDisplayName="James"
-        recieverDisplayName="Kevin"
+        giverDisplayName="James Long Lee"
+        recieverDisplayName="Kevin Pog Lueng"
         item={{ id: "1", display_name: "Coffee" }}
       />
     );
+    expect(wrapper.find("#favourAvatar").at(0).text()).toEqual("JL");
+    expect(wrapper.find("#favourAvatar").at(1).text()).toEqual("KL");
+    expect(wrapper.find("#favourAvatarText").at(0).text()).toEqual(
+      "James Long Lee"
+    );
+    expect(wrapper.find("#favourAvatarText").at(1).text()).toEqual(
+      "Kevin Pog Lueng"
+    );
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should render correctly with no reciever", () => {
+    const wrapper = shallow(
+      <IouFavour
+        giverDisplayName="James Long Lee"
+        recieverDisplayName=""
+        item={{ id: "1", display_name: "Coffee" }}
+      />
+    );
+    expect(wrapper.find("#favourAvatar").at(0).text()).toEqual("JL");
+    expect(wrapper.find("#favourAvatar").at(1).text()).toEqual("");
+    expect(wrapper.find("#favourAvatarText").at(0).text()).toEqual(
+      "James Long Lee"
+    );
+    expect(wrapper.find("#favourAvatarText").at(1).text()).toEqual(
+      "No Reciever"
+    );
   });
 });
