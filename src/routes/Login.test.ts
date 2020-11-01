ed47f3d6f89e346f61f9e234cb1287411d8322f5
Christopher Kesoglou <37316962+ckesoglou@users.noreply.github.com>
2020-11-01 17:19:48 +1100

    Fixed backend authentication tests to point to local node instance

--- src/routes/Login.test.ts
+++ src/routes/Login.test.ts
@@ -1,11 +1,9 @@
-/// <reference types="jest" />
-
+import app from "../Server";
+import request from "supertest";
 import sequelize from "@daos/DBInstance";
 import { deleteAllTokens } from "@daos/Tokens";
 import { deleteAllUsers } from "@daos/Users";
 
-const request = require("supertest");
-const APP = "https://ioweyou.tech";
 const TESTUSER = {
   username: "testunittestuser",
   displayName: "testunittestuser",
@@ -17,7 +15,7 @@ beforeAll(async () => {
   await sequelize.sync();
 
   // Create test user in database
-  await request(APP)
+  await request(app)
     .post("/api/signup")
     .send({
       username: `${TESTUSER.username}`,
@@ -37,36 +35,36 @@ afterAll(async () => {
 
 describe("Login endpoint", () => {
   it("should login user with correct credentials", async () => {
-    const res = await request(APP)
+    const res = await request(app)
       .post("/api/login")
       .send({
         username: `${TESTUSER.username}`,
         password: `${TESTUSER.password}`,
       });
-    expect(res.statusCode).toEqual(200);
+    expect(res.status).toEqual(200);
   });
 
   describe("Unauthorised", () => {
     it("should return error with wrong password", async () => {
-      const res = await request(APP)
+      const res = await request(app)
         .post("/api/login")
         .send({
           username: `${TESTUSER.username}`,
           password: "wrongpassword",
         });
-      expect(res.statusCode).toEqual(401);
+      expect(res.status).toEqual(401);
       expect(res.body).toHaveProperty("errors");
       expect(res.body.errors).toContain("Username or password was incorrect");
     });
 
     it("should return error with nonexistent username", async () => {
-      const res = await request(APP)
+      const res = await request(app)
         .post("/api/login")
         .send({
           username: "seanasdfasdfasdf",
           password: `${TESTUSER.password}`,
         });
-      expect(res.statusCode).toEqual(401);
+      expect(res.status).toEqual(401);
       expect(res.body).toHaveProperty("errors");
       expect(res.body.errors).toContain("Username or password was incorrect");
     });
@@ -74,13 +72,13 @@ describe("Login endpoint", () => {
 
   describe("Validation", () => {
     it("should validate username max field length", async () => {
-      const res = await request(APP)
+      const res = await request(app)
         .post("/api/login")
         .send({
           username: "seanmorethan16characters",
           password: `${TESTUSER.password}`,
         });
-      expect(res.statusCode).toEqual(400);
+      expect(res.status).toEqual(400);
       expect(res.body).toHaveProperty("errors");
       expect(res.body.errors).toContain(
         '"username" length must be less than or equal to 16 characters long'
@@ -88,13 +86,13 @@ describe("Login endpoint", () => {
     });
 
     it("should validate username min field length", async () => {
-      const res = await request(APP)
+      const res = await request(app)
         .post("/api/login")
         .send({
           username: "s",
           password: `${TESTUSER.password}`,
         });
-      expect(res.statusCode).toEqual(400);
+      expect(res.status).toEqual(400);
       expect(res.body).toHaveProperty("errors");
       expect(res.body.errors).toContain(
         '"username" length must be at least 2 characters long'
@@ -104,13 +102,13 @@ describe("Login endpoint", () => {
     it("should validate username field valid characters", async () => {
       const nonAlphaNumeric = "!@#$%^&*()_+-=`~[{]}\\|;:'\",<.>/?";
       for (let character of nonAlphaNumeric) {
-        const res = await request(APP)
+        const res = await request(app)
           .post("/api/login")
           .send({
             username: character,
             password: `${TESTUSER.password}`,
           });
-        expect(res.statusCode).toEqual(400);
+        expect(res.status).toEqual(400);
         expect(res.body).toHaveProperty("errors");
         expect(res.body.errors).toContain(
           '"username" must only contain alpha-numeric characters'
@@ -119,12 +117,12 @@ describe("Login endpoint", () => {
     });
 
     it("should validate password field", async () => {
-      const res = await request(APP)
+      const res = await request(app)
         .post("/api/login")
         .send({
           username: `${TESTUSER.username}`,
         });
-      expect(res.statusCode).toEqual(400);
+      expect(res.status).toEqual(400);
       expect(res.body).toHaveProperty("errors");
       expect(res.body.errors).toContain('"password" is required');
     });
--- src/routes/SignUp.test.ts
+++ src/routes/SignUp.test.ts
@@ -1,9 +1,13 @@
-/// <reference types="jest" />
-
+import app from "../Server";
+import request from "supertest";
 import sequelize from "@daos/DBInstance";
-import { deleteAllUsers } from "@daos/Users";
+import { createUser, deleteAllUsers } from "@daos/Users";
 
-import { request, APP, TESTUSER, deleteTestUsers } from "@shared/test.config";
+const TESTUSER = {
+  username: "testunittestuser",
+  displayName: "testunittestuser",
+  password: "Testunittestuser!",
+};
 
 beforeAll(async () => {
   await sequelize.drop();
@@ -21,49 +25,46 @@ afterAll(async () => {
 describe("Signup endpoint", () => {
   describe("Successful signup", () => {
     it("should sign up new user", async () => {
-      const res = await request(APP)
+      const res = await request(app)
         .post("/api/signup")
         .send({
-          username: `${TESTUSER.username}`,
+          username: TESTUSER.username,
           displayName: `${TESTUSER.displayName}`,
           password: `${TESTUSER.password}`,
         });
-      expect(res.statusCode).toEqual(201);
-    });
-    it("should login user with sign up credentials", async () => {
-      const res = await request(APP)
-        .post("/api/login")
-        .send({
-          username: `${TESTUSER.username}`,
-          password: `${TESTUSER.password}`,
-        });
-      expect(res.statusCode).toEqual(200);
+      expect(res.status).toEqual(201);
     });
   });
 
   describe("Validation", () => {
     it("should validate usernames already taken", async () => {
-      const res = await request(APP)
+      await createUser(
+        TESTUSER.username,
+        TESTUSER.displayName,
+        "password hash"
+      );
+
+      const res = await request(app)
         .post("/api/signup")
         .send({
           username: `${TESTUSER.username}`,
           displayName: `${TESTUSER.displayName}`,
           password: `${TESTUSER.password}`,
         });
-      expect(res.statusCode).toEqual(422);
+      expect(res.status).toEqual(422);
       expect(res.body).toHaveProperty("errors");
       expect(res.body.errors).toContain("Username already taken.");
     });
 
     it("should validate username max field length", async () => {
-      const res = await request(APP)
+      const res = await request(app)
         .post("/api/signup")
         .send({
           username: "testmorethan16characters",
           displayName: `${TESTUSER.displayName}`,
           password: `${TESTUSER.password}`,
         });
-      expect(res.statusCode).toEqual(400);
+      expect(res.status).toEqual(400);
       expect(res.body).toHaveProperty("errors");
       expect(res.body.errors).toContain(
         '"username" length must be less than or equal to 16 characters long'
@@ -71,14 +72,14 @@ describe("Signup endpoint", () => {
     });
 
     it("should validate username min field length", async () => {
-      const res = await request(APP)
+      const res = await request(app)
         .post("/api/signup")
         .send({
           username: "s",
           displayName: `${TESTUSER.displayName}`,
           password: `${TESTUSER.password}`,
         });
-      expect(res.statusCode).toEqual(400);
+      expect(res.status).toEqual(400);
       expect(res.body).toHaveProperty("errors");
       expect(res.body.errors).toContain(
         '"username" length must be at least 2 characters long'
@@ -88,14 +89,14 @@ describe("Signup endpoint", () => {
     it("should validate username field valid characters", async () => {
       const nonAlphaNumeric = "!@#$%^&*()_+-=`~[{]}\\|;:'\",<.>/?";
       for (let character of nonAlphaNumeric) {
-        const res = await request(APP)
+        const res = await request(app)
           .post("/api/signup")
           .send({
             username: character,
             displayName: `${TESTUSER.displayName}`,
             password: `${TESTUSER.password}`,
           });
-        expect(res.statusCode).toEqual(400);
+        expect(res.status).toEqual(400);
         expect(res.body).toHaveProperty("errors");
         expect(res.body.errors).toContain(
           '"username" must only contain alpha-numeric characters'
@@ -104,25 +105,25 @@ describe("Signup endpoint", () => {
     });
 
     it("should validate password field", async () => {
-      const res = await request(APP)
+      const res = await request(app)
         .post("/api/signup")
         .send({
           username: `${TESTUSER.username}`,
           displayName: `${TESTUSER.displayName}`,
         });
-      expect(res.statusCode).toEqual(400);
+      expect(res.status).toEqual(400);
       expect(res.body).toHaveProperty("errors");
       expect(res.body.errors).toContain('"password" is required');
     });
 
     it("should validate display name field", async () => {
-      const res = await request(APP)
+      const res = await request(app)
         .post("/api/signup")
         .send({
           username: `${TESTUSER.username}`,
           password: `${TESTUSER.password}`,
         });
-      expect(res.statusCode).toEqual(400);
+      expect(res.status).toEqual(400);
       expect(res.body).toHaveProperty("errors");
       expect(res.body.errors).toContain('"displayName" is required');
     });
