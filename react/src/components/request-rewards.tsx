52050dc5ba2800b40b26802c70ba1d60ea112e01
Christopher Kesoglou <37316962+ckesoglou@users.noreply.github.com>
2020-11-01 14:01:32 +1100

    Removed unnecessary import on request-rewards.tsx

--- react/src/components/request-rewards.tsx
+++ react/src/components/request-rewards.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../assets/css/iou-request.css";
-import { requestEndpoint, itemEndpoint } from "../api/endpoints";
+import { requestEndpoint } from "../api/endpoints";
 import RequestReward from "./request-reward";
 import { Redirect } from "react-router-dom";
 import {
