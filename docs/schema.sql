e2b38d59eb4fefa6a95a29cd7d4038c826bd6276
Christopher Kesoglou <37316962+ckesoglou@users.noreply.github.com>
2020-11-02 21:22:42 +1100

    Renamed requests table to offers in leaderboard view

--- docs/schema.sql
+++ docs/schema.sql
@@ -86,22 +86,22 @@ CREATE VIEW leaderboard AS (
 			ious.is_claimed = 1
 		UNION
 
-		/* 1 point for any request you have published */
+		/* 1 point for any request (offer) you have published */
 		SELECT
-			requests.author AS username,
+			offers.author AS username,
 			1 AS points
 		FROM
-			requests
+			offers
 		UNION
 
-		/* 3 points for any request you have completed */
+		/* 3 points for any request (offer) you have completed */
 		SELECT
-			requests.completed_by AS username,
+			offers.completed_by AS username,
 			3 AS points
 		FROM
-			requests
+			offers
 		WHERE
-			requests.is_completed = 1
+			offers.is_completed = 1
 	) activityLog
 		ON users.username = activityLog.username
 
