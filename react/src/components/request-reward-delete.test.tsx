caf0a300a0bb07eb7415194e4088f5a14fbc33ef
Piploop <james.lee9112000@gmail.com>
2020-11-02 22:57:20 +1100

    Fix unit tests to include auto-refresh functionality

--- react/src/components/request-reward-delete.test.tsx
+++ react/src/components/request-reward-delete.test.tsx
@@ -4,13 +4,35 @@ import RewardDelete from "./request-reward-delete";
 
 describe("<Iou />", () => {
   it("should render correctly", () => {
-    const wrapper = shallow(<RewardDelete requestID="1" rewardID="1" />);
+    const wrapper = shallow(
+      <RewardDelete
+        requestID="1"
+        rewardID="1"
+        refreshTable={() => {
+          return "substitute test function";
+        }}
+        closeModal={() => {
+          return "substitute test function";
+        }}
+      />
+    );
     expect(wrapper).toMatchSnapshot();
   });
 
   it("should call the delete reward API endpoint when clicked", () => {
     const spy = jest.spyOn(RewardDelete.prototype, "deleteReward");
-    const wrapper = mount(<RewardDelete requestID="1" rewardID="1" />);
+    const wrapper = mount(
+      <RewardDelete
+        requestID="1"
+        rewardID="1"
+        refreshTable={() => {
+          return "substitute test function";
+        }}
+        closeModal={() => {
+          return "substitute test function";
+        }}
+      />
+    );
     wrapper.find("#requestItem").simulate("click");
     expect(spy).toHaveBeenCalledTimes(1);
   });
